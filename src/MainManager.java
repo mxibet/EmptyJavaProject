1994c1cf37e170581d7cb80ec88e06978dcebc50
mxibet <marco.scibetta@gmail.com>
2018-12-02 17:29:09 +0100

    Update MainManager.java

--- src/MainManager.java
+++ src/MainManager.java
@@ -3,7 +3,7 @@ public class MainManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello GIT");
+		System.out.println("Hello GIT ble ble ble");
 	}
 
 }
